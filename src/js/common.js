08037976127859afc024776e950218a9b4077efd
Evgeny Zhernoklev <evgenyzhernoklev@MacBook-Pro-Evgeny.local>
2017-07-18 23:16:55 +0300

    Add empty block

--- src/js/common.js
+++ src/js/common.js
@@ -2,5 +2,20 @@ $(document).ready(function() {
   var $window = $(window),
       $body = $('body');
 
-  
+
+
+  $('.first-enter-close').one('click', function(e) {
+    e.preventDefault();
+    $body.removeClass('first-enter');
+  });
+
+
+  // full height in empty block
+  function fullHeight() {
+    var newHeight = $window.height() - $('.header').height();
+
+    $('.full-height').height(newHeight);
+  }
+
+  $window.on('load resize', fullHeight);
 });
