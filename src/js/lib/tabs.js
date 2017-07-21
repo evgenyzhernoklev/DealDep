04d96e36181777e6ea65ecb11eae172f712611bb
Evgeny Zhernoklev <evgenyzhernoklev@MacBook-Pro-Evgeny.local>
2017-07-21 12:00:45 +0300

    Add animations, fix styles on projects add page

--- src/js/lib/tabs.js
+++ src/js/lib/tabs.js
@@ -1,7 +1,8 @@
 var Tabs = function(container) {
   this.container = $(container);
   this.content = this.container.find('.tab-content');
-  this.links = this.container.find('.tab-link');
+  this.linksContainer = this.container.find('.tab-link-container');
+  this.links = this.linksContainer.find('.tab-link');
 
   this.init();
 };
@@ -20,14 +21,19 @@ Tabs.prototype.checkActiveContent = function () {
 
 Tabs.prototype.switchContent = function (e) {
   e.preventDefault();
-  var $target = $(e.target);
+  var $target = $(e.target),
+      linkContainerClass = $target.data('container-class');
 
   if ($target.hasClass('is-active')) {
     return;
   }
 
+
+  this.linksContainer.removeClass('slide');
   this.links.removeClass('is-active');
   this.content.removeClass('is-active');
+
+  this.linksContainer.addClass(linkContainerClass);
   $target.addClass('is-active');
   this.checkActiveContent();
 };
