89dba6690b88027548c97137fedca5084483a9e9
evgenyzhernoklev <oldschool127@mail.ru>
2017-11-20 12:28:43 +0300

    Update mobile header for account page

--- src/js/account.js
+++ src/js/account.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+  $hiddenText = $('.loginHeaderTooltip__hidden');
+
   $('.loginHeaderTooltip').on('click', function () {
     $(this).toggleClass('is-active');
+  });
+
+  $('.tab-link').on('click', function() {
+    $hiddenText.text($(this).text());
   });
 });
